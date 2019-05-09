cf60fb65a0d4464116f854efda98e1f16f26cec4
an31146 <richard.hammond@att.net>
2019-05-09 09:12:47 +0100

    Updated BinaryFileStreamIO.cs

--- mono/BinaryFileStreamIO.cs
+++ mono/BinaryFileStreamIO.cs
@@ -1,10 +1,13 @@
 ﻿// #!/usr/bin/mcs
 using System;
+using System.Diagnostics;
 using System.IO;
 
+#pragma warning disable CS0219
 class MyStream
 {
     private const string FILE_NAME = "Test.data";
+
     public static void Main(String[] args)
     {
         // Create the new, empty data file.
@@ -13,37 +16,70 @@ class MyStream
             Console.WriteLine("{0} already exists!", FILE_NAME);
             return;
         }
+
+        int COUNT = 1048576;
+        if (args.Length == 1)
+            COUNT = Int32.Parse(args[0]);
+
         FileStream fs = new FileStream(FILE_NAME, FileMode.CreateNew);
+        Stopwatch sw = new Stopwatch();
+
         // Create the writer for data.
         BinaryWriter w = new BinaryWriter(fs);
+        string strAscii = "";
 
-        for (int j = 0; j < 16777216; j++)
+        // Create a string of ASCII characters
+        for (int i = 32; i < 255; i++)
         {
-            // Write data to Test.data.
-            for (int i = 32; i < 255; i++)
-            {
-                w.Write((char)i);
-            }
+            strAscii += (char)i;
+        }
+        // Multiply and concatenate
+        strAscii += strAscii;
+        strAscii += strAscii;
+        
+        Console.WriteLine("Writing {0} bytes to file...", strAscii.Length * COUNT / 4);
+        sw.Start();
+        // Write the string for COUNT times to Test.data
+        for (int j = 0; j < COUNT / 4; j++)
+        {
+            w.Write(strAscii.ToCharArray());
+            Console.Write("{0:F1} %\r", (float)j / (COUNT / 400.0f));
         }
         w.Close();
         fs.Close();
 
+        sw.Stop();
+        
+        Console.WriteLine("\nElapsed time: {0} ms\n", sw.ElapsedMilliseconds);
+
         // Create the reader for data.
         fs = new FileStream(FILE_NAME, FileMode.Open, FileAccess.Read);
-        BinaryReader r = new BinaryReader(fs);
 
+        BinaryReader r = new BinaryReader(fs);
+        char[] charArray = new char[1024];
+
+        Console.WriteLine("Reading from file...");
+
+        sw.Restart();
         while (fs.Position < fs.Length)
         {
             // Read data from Test.data.
             for (int i = 32; i < 255; i++)
             {
-                Console.Write(r.ReadChar());
+                //Console.Write(r.ReadChar());
+                //r.ReadChar();			// Elapsed time: 115080 ms
+                charArray = r.ReadChars(1024);
+                Console.Write("{0:F1} %\r", (float)fs.Position / fs.Length * 100.0f);
             }
         }
-        Console.WriteLine();
+        sw.Stop();
+
+        Console.WriteLine("\nElapsed time: {0} ms\n", sw.ElapsedMilliseconds);
+
         r.Close();
         fs.Close();
 
         File.Delete("Test.data");
     }
 }
+#pragma warning restore CS0219
