e325e64d551cc2ad7db50c04c4a28ed290c4e172
an31146 <richard.hammond@att.net>
2019-06-17 09:56:37 +0100

    failing Hired assessments... :-(

---
+++ mono/parantheses.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+public class Challenge {
+    public static long Solution(string parentheses, long index) {
+        // Type your solution here
+        if (parentheses[(int)index].Equals(')'))
+            return -1;
+        
+        long p = 0, res = 0;
+        for (int i = (int)index; i < parentheses.Length; i++)
+        {
+            if (parentheses[i].Equals('('))
+            {
+                Console.Write("+");
+                p++;
+            }
+            else if (parentheses[i].Equals(')'))
+            {
+                Console.Write('-');
+                p--;
+            }
+            if (p == 0 && i > index)
+                res = i;
+            //Console.WriteLine(i);
+        }
+        return res;
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(Solution("((()())())", 0L));
+    }
+}
