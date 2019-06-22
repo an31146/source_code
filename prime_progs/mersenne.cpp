c5da8937ef79d227fe4ac2656a15410ce62c0347
an31146 <richard.hammond@att.net>
2019-06-23 00:20:08 +0100

    updated mersenne & segmented_sieve but mersenne function isn't working.

--- prime_progs/mersenne.cpp
+++ prime_progs/mersenne.cpp
@@ -1,39 +1,31 @@
-﻿#include <iostream>
+﻿/*
+ * g++ -Wall -O2 -std=c++11 -o mersenne.exe mersenne.cpp -lmpir
+ */
+#include <iostream>
+#include <iomanip>
 #include <chrono>
 #include <cstdlib>
 #include <ctime>
-#include <array>
+#include <cmath>
+#include <vector>
+#include <conio.h>
 #include <mpir.h>
 
 using namespace std;
 
-#define LIMIT 49341
-array<unsigned int, LIMIT> primes;
-
-array<unsigned int, LIMIT> sieve()
-{
-    unsigned p = 0;
-    
-    //memset(primes, 0, sizeof(primes));
-
-    primes[p] = 2;
-    while (primes[p] <= LIMIT) 
-    {
-        for (unsigned i = primes[p]*primes[p]; i <= LIMIT; i+=primes[p])
-            primes[i] = 1;
-        primes[++p] = primes[p-1] + 1;
-        for (; primes[p] <= LIMIT && primes[primes[p]] == 1; primes[p]++);      //find next prime (where s[p]==0)
-    }
-    return primes;
-}
+#define L1D_CACHE_SIZE 32768
+#define LIMIT 1000000
+//array<unsigned long, LIMIT> primes;
+vector<unsigned> primes;
 
 // Use LucasLehmer to determine if 2^n-1 is prime
-bool LucasLehmer(unsigned int n)
+bool LucasLehmer(unsigned n)
 {
     mpz_t seed, div, i, ONE;
     
     mpz_inits(seed, div, i, ONE, NULL);
     mpz_set_ui(seed, 4);
+    mpz_set_ui(ONE, 1);
 
     mpz_mul_2exp(div, ONE, n);          // div = 2^n
     mpz_sub_ui(div, div, 1L);           // div = 2^n - 1
@@ -49,12 +41,12 @@ bool LucasLehmer(unsigned int n)
     }
     
     bool bSeedIsZero = (seed == 0);
-    mpz_clears(seed, div, i, ONE);
+    mpz_clears(seed, div, i, ONE, NULL);
     
     return bSeedIsZero;
-}
+}   // LucasLehmer
 
-void Mersenne(unsigned int n)
+void Mersenne(unsigned n, vector<unsigned> &pr)
 {
     mpz_t PowerOf2Sub1, TWO;
     bool isMprime;
@@ -66,11 +58,11 @@ void Mersenne(unsigned int n)
     
     mpz_inits(PowerOf2Sub1, TWO, NULL);
     
-    for (unsigned int i = 0, x = 1; i < primes.size(); i++)
+    for (unsigned i = 0, x = 1; i < pr.size(); i++)
     {
         auto start = chrono::system_clock::now();
         {
-            isMprime = LucasLehmer(primes[i]);
+            isMprime = LucasLehmer(pr[i]);
         }
         auto end = chrono::system_clock::now();
         elapsed_seconds = end - start;
@@ -78,28 +70,133 @@ void Mersenne(unsigned int n)
         if (isMprime)
         {
             //mpz_mul_2exp (mpz t rop, const mpz t op1, mp bitcnt t op2)
-            mpz_mul_2exp(PowerOf2Sub1, TWO, primes[i]);
+            mpz_mul_2exp(PowerOf2Sub1, TWO, pr[i]);
             mpz_sub_ui(PowerOf2Sub1, PowerOf2Sub1, 1);
-            auto strPtr = strPowerOf2Sub1.c_str();
-            mpz_get_str(strPtr, 10, PowerOf2Sub1);
+
+            auto strPtr1 = (char *)strPowerOf2Sub1.c_str();
+            mpz_get_str(strPtr1, 10, PowerOf2Sub1);
             
             if (x < 10)
                 cout << "M[" << primes[i] << "] = " << strPowerOf2Sub1;
             else
                 cout << "M[" << primes[i] << "] = " << strPowerOf2Sub1.substr(0, 12) << "..." << strPowerOf2Sub1.substr(strPowerOf2Sub1.size() - 12, 12) << endl;
             x++;
-            cout << "elapsed time: " << elapsed_seconds << " s" << endl;
-        }
+            cout << "elapsed time: " << elapsed_seconds.count() << " s" << endl;
+        }   // if (isMprime)
         if (n < x)
         {
             break;
         }
+    }   // for
+    mpz_clears(PowerOf2Sub1, TWO, NULL);
+}   // Mersenne
+
+/*
+unsigned sieve(array<unsigned long, LIMIT> &pr)
+{
+    unsigned p = 0;
+    
+    //memset(primes, 0, sizeof(primes));
+
+    pr[p] = 2;
+    while (pr[p] <= LIMIT) 
+    {
+        for (unsigned long i = pr[p]*pr[p]; i <= LIMIT; i += pr[p])
+            pr[i] = 1;
+        pr[++p] = pr[p-1]+1;
+        while (pr[p] <= LIMIT && pr[pr[p]] == 1)
+            //find next prime (where s[p]==0)
+            pr[p]++;
     }
+    return p;
+}
+*/
+
+/// Generate primes using the segmented sieve of Eratosthenes.
+/// This algorithm uses O(n log log n) operations and O(sqrt(n)) space.
+/// @param limit         Sieve primes <= limit.
+/// @param segment_size  Size of the sieve array in bytes.
+///
+int64_t segmented_sieve(int64_t limit, unsigned segment_size = L1D_CACHE_SIZE)
+{
+    unsigned _sqrt = (unsigned) sqrt((double) limit);
+    int64_t count = (limit < 2) ? 0 : 1;
+    int64_t s = 2;
+    int64_t n = 3;
+
+    // vector used for sieving
+    vector<char> segment(segment_size);
+
+    // generate small primes <= sqrt
+    vector<char> is_prime(_sqrt + 1, 1);
+    for (unsigned i = 2; i * i <= _sqrt; i++)
+        if (is_prime[i])
+            for (unsigned j = i * i; j <= _sqrt; j += i)
+                is_prime[j] = 0;
+
+    vector<unsigned> next;
+
+    for (int64_t low = 0; low <= limit; low += segment_size)
+    {
+        fill(segment.begin(), segment.end(), 1);
+
+        // current segment = interval [low, high]
+        int64_t high = min(low + segment_size - 1, limit);
+
+        // store small primes needed to cross off multiples
+        for (; s * s <= high; s++)
+            if (is_prime[s])
+            {
+                primes.push_back((unsigned) s);
+                next.push_back((unsigned)(s * s - low));
+            }
+
+        // segmented sieve of Eratosthenes
+        for (size_t i = 1; i < primes.size(); i++)
+        {
+            unsigned j = next[i];
+            for (unsigned k = primes[i] * 2; j < segment_size; j += k)
+                segment[j] = 0;
+            next[i] = j - segment_size;
+        }
+        for (; n <= high; n += 2)
+            count += segment[n - low];
+    }
+    cout << endl << count << " primes found." << endl << endl;
+
+    return count;
 }
 
 int main(int argc, char** argv)
 {
     //
+    unsigned n;
+    chrono::duration<double> elapsed_seconds;
+    auto start = chrono::system_clock::now();
+    {
+        n = segmented_sieve(LIMIT);
+    }
+    auto end = chrono::system_clock::now();
+    elapsed_seconds = end - start;
+
+    cout << "sieve time: " << setprecision(0) << elapsed_seconds.count() * 1000.0f << " ms." << endl << endl;
+    
+    cout << "Print primes? [Y/n]" << endl;
+    char c = _getch();
+    //cout << (int)c << endl;
+    
+    if (c == 'Y' || c == 'y' || c == 0xd)
+    {
+        for (auto p : primes)
+            if (p > 1)
+                cout << setw(8) << p;
+            else
+                break;
+    }
+            
+    cout << endl << "Vector contains " << n << " primes." << endl << endl;
+    
+    Mersenne(29, primes);
     
     return EXIT_SUCCESS;
 }
--- prime_progs/segmented_sieve.cpp
+++ prime_progs/segmented_sieve.cpp
@@ -6,13 +6,15 @@
 ///         Intel Core i7-4770 CPU (3.4 GHz) from 2013.
 ///         This is free software released into the public domain.
 
-#include <iostream>
-#include <iomanip>
 #include <algorithm>
 #include <cmath>
-#include <vector>
+#include <cstdint>
 #include <cstdlib>
-#include <stdint.h>
+#include <iostream>
+#include <iomanip>
+#include <vector>
+
+#include <conio.h>
 #include <time.h>
 #include <windows.h>
 
@@ -27,82 +29,98 @@ using namespace std;
 ///
 void segmented_sieve(int64_t limit, int segment_size = L1D_CACHE_SIZE)
 {
-  int sqrt = (int) std::sqrt((double) limit);
-  //double twin_prime_const = 1.0d;
-  int64_t count = (limit < 2) ? 0 : 1;
-  int64_t s = 2;
-  int64_t n = 3;
+    int sqrt = (int) std::sqrt((double) limit);
+    //double twin_prime_const = 1.0d;
+    int64_t count = (limit < 2) ? 0 : 1;
+    int64_t s = 2;
+    int64_t n = 3;
 
-  // vector used for sieving
-  vector<char> segment(segment_size);
+    // vector used for sieving
+    vector<char> segment(segment_size);
 
-  // generate small primes <= sqrt
-  vector<char> is_prime(sqrt + 1, 1);
-  for (int i = 2; i * i <= sqrt; i++)
-    if (is_prime[i])
-      for (int j = i * i; j <= sqrt; j += i)
-        is_prime[j] = 0;
+    // generate small primes <= sqrt
+    vector<char> is_prime(sqrt + 1, 1);
+    for (int i = 2; i * i <= sqrt; i++)
+        if (is_prime[i])
+            for (int j = i * i; j <= sqrt; j += i)
+                is_prime[j] = 0;
 
-  vector<int> primes;
-  vector<int> next;
+    vector<int> primes;
+    vector<int> next;
 
-  for (int64_t low = 0; low <= limit; low += segment_size)
-  {
-    fill(segment.begin(), segment.end(), 1);
-
-    // current segment = interval [low, high]
-    int64_t high = min(low + segment_size - 1, limit);
-
-    // store small primes needed to cross off multiples
-    for (; s * s <= high; s++)
-      if (is_prime[s])
-      {
-        primes.push_back((int) s);
-          next.push_back((int)(s * s - low));
-      }
-
-    // segmented sieve of Eratosthenes
-    for (size_t i = 1; i < primes.size(); i++)
+    for (int64_t low = 0; low <= limit; low += segment_size)
     {
-      int j = next[i];
-      for (int k = primes[i] * 2; j < segment_size; j += k)
-        segment[j] = 0;
-      next[i] = j - segment_size;
-    }
-    for (; n <= high; n += 2)
-        count += segment[n - low];
+        fill(segment.begin(), segment.end(), 1);
+
+        // current segment = interval [low, high]
+        int64_t high = min(low + segment_size - 1, limit);
+
+        // store small primes needed to cross off multiples
+        for (; s * s <= high; s++)
+            if (is_prime[s])
+            {
+                primes.push_back((int) s);
+                next.push_back((int)(s * s - low));
+            }
+
+        // segmented sieve of Eratosthenes
+        for (size_t i = 1; i < primes.size(); i++)
+        {
+            int j = next[i];
+            for (int k = primes[i] * 2; j < segment_size; j += k)
+                segment[j] = 0;
+            next[i] = j - segment_size;
+        }
+        for (; n <= high; n += 2)
+            count += segment[n - low];
 //      if (segment[n - low])
 //      {
-//        //cout << setw(10) << n;
-//        count++;
-//        twin_prime_const *= 1.0d - 1.0d / (double)((n-1) * (n-1));
+//          cout << setw(10) << n;
+//          count++;
+//          twin_prime_const *= 1.0d - 1.0d / (double)((n-1) * (n-1));
 //      }  
-  }
+    }
 
-  cout << endl << count << " primes found." << endl;
-  cout.precision(18);
-  //DebugBreak();
-  //cout << "twin prime constant: " << twin_prime_const << endl;
+    cout << endl << count << " primes found." << endl;
+
+    cout << "Print primes? [Y/n]" << endl;
+    char c = _getch();
+    //cout << (int)c << endl;
+    
+    if (c == 'Y' || c == 'y' || c == 0xd)
+    {
+        cout.precision(18);
+        for (auto p : primes)
+            if (p > 1)
+                cout << setw(8) << p;
+            else
+                break;
+    }
+
+    //DebugBreak();
+    //cout << "twin prime constant: " << twin_prime_const << endl;
 }
 
 int main(int argc, char** argv)
 {
-  // generate the primes below this number
-  int64_t limit = 100000000;
-  int start, stop;
-  
-  if (argc >= 2)
-    limit = atoll(argv[1]);
+    // generate the primes below this number
+    int64_t limit = 100000000;
+    int start, stop;
 
-  int size = L1D_CACHE_SIZE;
-  if (argc >= 3)
-    size = atoi(argv[2]);
+    if (argc >= 2)
+        limit = atoll(argv[1]);
 
-  start = clock();
-  segmented_sieve(limit, size);
-  stop = clock();
-  
-  cout << "sieve time: " << (stop-start) << " ms." << endl;
-  return 0;
+    int size = L1D_CACHE_SIZE;
+    if (argc >= 3)
+        size = atoi(argv[2]);
+
+    start = clock();
+    {        
+        segmented_sieve(limit, size);
+    }
+    stop = clock();
+
+    cout << "sieve time: " << (stop-start) << " ms." << endl;
+    return 0;
 }
 
