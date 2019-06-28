a98547c045328759a873e713e72b816ad66e5486
an31146 <richard.hammond@att.net>
2019-06-28 19:37:35 +0100

    copied mersenne.cpp & segmented_sieve.cpp

--- prime_progs/segmented_sieve.cpp
+++ prime_progs/segmented_sieve.cpp
@@ -6,14 +6,17 @@
 ///         Intel Core i7-4770 CPU (3.4 GHz) from 2013.
 ///         This is free software released into the public domain.
 
-#include <iostream>
-#include <iomanip>
 #include <algorithm>
 #include <cmath>
-#include <vector>
+#include <cstdint>
 #include <cstdlib>
-#include <stdint.h>
+#include <iostream>
+#include <iomanip>
+#include <vector>
+//
+#include <conio.h>
 #include <time.h>
+#include <windows.h>
 
 const int L1D_CACHE_SIZE = 32768;
 
@@ -24,89 +27,117 @@ using namespace std;
 /// @param limit         Sieve primes <= limit.
 /// @param segment_size  Size of the sieve array in bytes.
 ///
-void segmented_sieve(int64_t limit, int segment_size = L1D_CACHE_SIZE)
+vector<int> segmented_sieve(int64_t limit, int segment_size = L1D_CACHE_SIZE)
 {
-  int sqrt = (int) std::sqrt((double) limit);
-  //double twin_prime_const = 1.0d;
-  int64_t count = (limit < 2) ? 0 : 1;
-  int64_t s = 2;
-  int64_t n = 3;
+    int _sqrt = (int) sqrt((double) limit);
+    //double twin_prime_const = 1.0d;
+    int64_t count = (limit < 2) ? 0 : 1;
+    int64_t s = 2;
+    int64_t n = 3;
 
-  // vector used for sieving
-  std::vector<char> segment(segment_size);
+    // vector used for sieving
+    vector<char> segment(segment_size);
 
-  // generate small primes <= sqrt
-  std::vector<char> is_prime(sqrt + 1, 1);
-  for (int i = 2; i * i <= sqrt; i++)
-    if (is_prime[i])
-      for (int j = i * i; j <= sqrt; j += i)
-        is_prime[j] = 0;
+    // generate small primes <= sqrt
+    vector<char> is_prime(_sqrt + 1, 1);
+    for (int i = 2; i * i <= _sqrt; i++)
+        if (is_prime[i])
+            for (int j = i * i; j <= _sqrt; j += i)
+                is_prime[j] = 0;
 
-  std::vector<int> primes;
-  std::vector<int> next;
+    vector<int> primes;
+    vector<int> next;
+    vector<int> final_primes;
 
-  for (int64_t low = 0; low <= limit; low += segment_size)
-  {
-    std::fill(segment.begin(), segment.end(), 1);
-
-    // current segment = interval [low, high]
-    int64_t high = std::min(low + segment_size - 1, limit);
-
-    // store small primes needed to cross off multiples
-    //cout << setw(8) << s;
-    for (; s * s <= high; s++)
-      if (is_prime[s])
-      {
-        //cout << s << endl;
-        primes.push_back((int) s);
-          next.push_back((int)(s * s - low));
-      }
-
-    // segmented sieve of Eratosthenes
-    for (std::size_t i = 1; i < primes.size(); i++)
+    for (int64_t low = 0; low <= limit; low += segment_size)
     {
-      int j = next[i];
-      for (int k = primes[i] * 2; j < segment_size; j += k)
-        segment[j] = 0;
-      next[i] = j - segment_size;
-    }
-    //cout << setw(12) << n;
-    for (; n <= high; n += 2)
-      count+=(segment[n - low]);
-/*
-      if (segment[n - low])
-      {
-        cout << setw(10) << n;
-        count++;
-        //twin_prime_const *= 1.0 - 1.0 / (double)((n-1) * (n-1));
-      }  
-*/
-  }
+        fill(segment.begin(), segment.end(), 1);
 
-  cout << endl << count << " primes found." << endl;
-  cout.precision(18);
-  //DebugBreak();
-  //cout << "twin prime constant: " << twin_prime_const << endl;
+        // current segment = interval [low, high]
+        int64_t high = min(low + segment_size - 1, limit);
+
+        // store small primes needed to cross off multiples
+        for (; s * s <= high; s++)
+            if (is_prime[s])
+            {
+                primes.push_back((int) s);
+                //cout << setw(8) << s;
+                next.push_back((int)(s * s - low));
+                //cout << setw(8) << (s * s - low);
+            }
+
+        // segmented sieve of Eratosthenes
+        for (size_t i = 1; i < primes.size(); i++)
+        {
+            int j = next[i];
+            for (int k = primes[i] * 2; j < segment_size; j += k)
+                segment[j] = 0;
+            next[i] = j - segment_size;
+        }
+        
+        for (; n <= high; n += 2)
+        {
+            //count += segment[n - low];
+            if (segment[n - low] == 1)
+            {
+                count++;
+                final_primes.push_back(n);
+            }
+        }
+            
+//      if (segment[n - low])
+//      {
+//          cout << setw(10) << n;
+//          count++;
+//          twin_prime_const *= 1.0d - 1.0d / (double)((n-1) * (n-1));
+//      }  
+    }
+    auto it = final_primes.begin();
+    final_primes.insert(it, 2);
+
+    cout << endl << count << " primes found." << endl;
+
+    //DebugBreak();
+    //cout << "twin prime constant: " << twin_prime_const << endl;
+    return final_primes;
 }
 
 int main(int argc, char** argv)
 {
-  // generate the primes below this number
-  int64_t limit = 100000000;
-  int start, stop;
-  
-  if (argc >= 2)
-    limit = atoll(argv[1]);
+    // generate the primes below this number
+    int64_t LIMIT = 100000000;
+    vector<int> _primes;
 
-  int size = L1D_CACHE_SIZE;
-  if (argc >= 3)
-    size = atoi(argv[2]);
+    if (argc >= 2)
+        LIMIT = atoll(argv[1]);
 
-  start = clock();
-  segmented_sieve(limit, size);
-  stop = clock();
-  
-  cout << "sieve time: " << (stop-start) / 1000 << " ms." << endl;
-  return 0;
+    int size = L1D_CACHE_SIZE;
+    if (argc >= 3)
+        size = atoi(argv[2]);
+
+    auto start = clock();
+    {        
+        _primes = segmented_sieve(LIMIT, size);
+    }
+    auto stop = clock();
+
+    cout << "vector size() = " << _primes.size() << endl;
+    cout << "sieve time: " << (stop-start) << " ms." << endl << endl;
+    
+    cout << "Print primes? [Y/n]" << endl;
+    char c = _getch();
+    //cout << (int)c << endl;
+    
+    if (c == 'Y' || c == 'y' || c == 0xd)
+    {
+        cout.precision(18);
+        for (auto p : _primes)
+            //if (p > 1)
+            cout << setw(8) << p;
+            //else
+            //    break;
+    }
+
+    return EXIT_SUCCESS;
 }
 
