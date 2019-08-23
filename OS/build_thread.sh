46745a23f0fd116c832648cd19b9eed6b41cb8fd
an31146 <richard.hammond@att.net>
2019-08-23 14:13:24 +0100

    Changes to be committed: 	modified:   OS/build_thread.sh

--- OS/build_thread.sh
+++ OS/build_thread.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-g++ -O2 -std=c++11 -o thread thread.cpp -lpthread
-g++ -O2 -std=c++14 -o shared_mutex shared_mutex.cpp -lpthread
+g++ -Wall -O2 -std=c++11 -o thread thread.cpp -lpthread
+g++ -Wall -O2 -std=c++14 -o shared_mutex shared_mutex.cpp -lpthread
