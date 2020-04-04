05ab6e028498f500b1fd1e949696c7b5b6d23a3e
Richard Hammond <richard.hammond@att.net>
2020-04-04 13:41:34 +0100

    where did parseInts.cpp come from?

--- mono/build.cmd
+++ mono/build.cmd
@@ -1,2 +1,2 @@
 @echo off
-"C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\Roslyn\csc.exe" /target:exe /platform:AnyCPU /out:%~n1.exe %1
+".\Microsoft.Net.Compilers.Toolset.3.5.0\tasks\net472\csc.exe" /target:exe /platform:AnyCPU /out:%~n1.exe %1
--- parseInts.cpp
+++ parseInts.cpp
@@ -9,33 +9,38 @@ vector<int> parseInts(string str) {
 	// Complete this function
     vector<int> v;
     unsigned i = 0;
-    while (i < str.size())
+    int nextComma = 0;
+	while (nextComma > -1)
     {
-        unsigned nextComma = str.find(',');
+        nextComma = str.find(',');
         string strInt = str.substr(i, nextComma);
+		str = str.substr(nextComma + 1);
         
-        cout << "parseInt: " << strInt << "\t nextComma: " << nextComma << endl;
+        cout << "parseInt: " << strInt << "\nnextComma: " << nextComma << endl;
         v.push_back(atoi(strInt.c_str()));
         
-        cout << "str: " << str << endl;
-        i = str.find(2, ',');
-        cout << "i: " << i << endl;
+        cout << "str: " << str << endl << endl;
+        //i = str.find(',', nextComma + 1);
+		//if (i > 0)
+			// nextComma = i;
+        //cout << "i: " << i << endl;
         
+		/*
         if (i == string::npos)
         {
             i++;
             break;
-        }
+        }*/
     }
     return v;
 }
 
 int main(int argc, char**argv) {
-    string str;
-    cin >> str;
+    string str = " 2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97";
+    //cin >> str;
     vector<int> integers = parseInts(str);
     for(unsigned i = 0; i < integers.size(); i++) {
-        cout << integers[i] << "\n";
+        cout << "integers[" << i << "]: " << integers[i] << endl;
     }
     
     return 0;
