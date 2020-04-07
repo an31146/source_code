3dcab9a7ae4c1c36ac5c2bd0e60bd4d8e36d0043
Richard Hammond <richard.hammond@att.net>
2020-04-07 21:20:23 +0100

    Transferring BigInteger byte structures between RSABigInt and BigInteger.cs
     Please enter the commit message for your changes. Lines starting
     with '#' will be ignored, and an empty message aborts the commit.
    
     Date:      Tue Apr 7 21:20:23 2020 +0100
    
     On branch windows
     Your branch is ahead of 'windows/windows' by 2 commits.
       (use "git push" to publish your local commits)
    
     Changes to be committed:
    	new file:   BigInteger.cs
    	new file:   Program/Program.cs
    	new file:   Program/Program.csproj
    	modified:   ../prime_progs/mpz_powm_neg-exp.cpp
    
     Changes not staged for commit:
    	modified:   ../prime_progs/mpz_powm_neg-exp.cpp
    
     Untracked files:
    	Program/bin/
    	Program/obj/

---
+++ mono/Program/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] primes = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
+            for (int i = 0; i < 2000; i++)
+            {
+                bool bPrime = true;
+                foreach (int p in primes)
+                    if (i % p == 0 && i != p || i == 1)
+                        bPrime = false;
+                if (bPrime)
+                    Console.WriteLine("[{0,4:D2}] Hello World!", i);
+            }
+        }
+    }
+}
--- prime_progs/mpz_powm_neg-exp.cpp
+++ prime_progs/mpz_powm_neg-exp.cpp
@@ -1,3 +1,6 @@
+/*
+ * g++ -Wall -O2 -std=c++11 -I C:\Tools\gmp-6.1.2 -o mpz_powm_neg-exp.exe mpz_powm_neg-exp.cpp -lgmpxx -lgmp
+ */
 #include <gmpxx.h>
 #include <iostream>
 
@@ -6,6 +9,9 @@ int main()
     mpz_class n(17), k(23), res;
     mpz_powm(res.get_mpz_t(), k.get_mpz_t(), mpz_class(-1).get_mpz_t(), n.get_mpz_t());
     std::cout << res << std::endl;
+    
+    n = mpz_class("8044519426324086992388511943860210371379357135842224134212814310730659405905187050236423326842895406851752905185507785094492055233367688297241115376133579", 10);
+    std::cout << "mpz_is_prime(" << n << "): " << mpz_probab_prime_p(n.get_mpz_t(), 23) << std::endl;
 
 	return 0;
 }
